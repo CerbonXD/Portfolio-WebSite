f267bdb1f5e66f830d288a616ca52b987bd71fc2
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2024-09-25 11:57:02 -0300

    Add filter button animation

--- src/components/FilterButton/FilterButton.jsx
+++ src/components/FilterButton/FilterButton.jsx
@@ -1,6 +1,11 @@
 export default function FilterButton({ label, isActive, onClick }) {
+    const buttonHoverCss = isActive ? " hover:bg-[#6A1FC0]/[.14]" : " hover:bg-white/[.08]"
+
+    const buttonCss = "border-white/[.16] border px-5 py-2 rounded-lg bg-none dark:text-white transition" + buttonHoverCss
+    const buttonActiveCss = "border-[#6A1FC0] border px-5 py-2 rounded-lg bg-none dark:text-[#6A1FC0] transition" + buttonHoverCss
+
     return (
-        <button onClick={onClick} className="border-white/[.16] border px-5 py-2 rounded-lg bg-none dark:text-white" style={isActive ? {color: "#6A1FC0", borderColor: "#6A1FC0"} : {}}>
+        <button onClick={onClick} className={isActive ? buttonActiveCss : buttonCss}>
             {label}
         </button>
     );
