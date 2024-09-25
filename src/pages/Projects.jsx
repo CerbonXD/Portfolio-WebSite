83aca9d52200ca03877d0dcf9f7a9af72455d44f
CerbonXD <felipecerboncinix@gmail.com>
2024-09-25 20:18:57 -0300

    Add a show more button to projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,12 +1,13 @@
 import Project from "../components/Project/Project.jsx";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import FilterButton from "../components/FilterButton/FilterButton.jsx";
 
 export default function Projects() {
     const [data, setData] = useState([]);
-    const [filter, setFilter] = useState('All'); // Manage the selected filter
+    const [filter, setFilter] = useState('All');
+    const [visibleProjects, setVisibleProjects] = useState(8);
 
-    const filters = ["All", "Sites", "Minecraft Mods"]
+    const filters = ["All", "Sites", "Minecraft Mods"];
 
     useEffect(() => {
         fetch('/projects.json')
@@ -14,6 +15,7 @@ export default function Projects() {
             .then(data => setData(data))
             .catch(error => console.error('Error fetching data:', error));
     }, []);
+
     const filteredProjects = data.filter(project => {
         switch (filter) {
             case "All": return true;
@@ -23,15 +25,19 @@ export default function Projects() {
         }
     });
 
+    const handleShowMore = () => {
+        setVisibleProjects(prevAmount => prevAmount + 8);
+    };
+
     return (
         <div className="mb-[4.375rem]">
             <div className="flex justify-center gap-2.5 my-[4.375rem]">
                 {filters.map((f) => (
-                    <FilterButton key={f} label={f} isActive={filter === f} onClick={() => setFilter(f)}></FilterButton>
+                    <FilterButton key={f} label={f} isActive={filter === f} onClick={() => {setFilter(f); setVisibleProjects(8)}} />
                 ))}
             </div>
             <div className="flex flex-wrap gap-7 mt-10 max-w-[67.75rem] mx-auto">
-                {filteredProjects.map((project) => (
+                {filteredProjects.slice(0, visibleProjects).map((project) => (
                     <Project
                         key={project.title}
                         title={project.title}
@@ -41,6 +47,13 @@ export default function Projects() {
                         badges={project.badges} />
                 ))}
             </div>
+            {visibleProjects < filteredProjects.length && (
+                <div className="flex justify-center mt-[4.375rem]">
+                    <button className="px-5 py-2 rounded-lg bg-white/[.04] dark:text-white hover:brightness-90" onClick={handleShowMore}>
+                        Show More
+                    </button>
+                </div>
+            )}
         </div>
     );
 }
