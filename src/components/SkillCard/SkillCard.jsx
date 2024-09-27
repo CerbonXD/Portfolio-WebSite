d8fd5dfe0229d75cfcd9e851003d3e49bd2ed70a
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2024-09-26 23:52:52 -0300

    Rewrite about me page and add a SkillCard component

---
+++ src/components/SkillCard/SkillCard.jsx
@@ -0,0 +1,19 @@
+import PropTypes from "prop-types";
+
+export default function SkillCard({ skillName, svg }) {
+    return (
+        <div className="group p-10 dark:bg-white/[.04] rounded-lg relative transform transition-transform hover:scale-125 shadow-lg dark:text-white">
+            <div className="group-hover:translate-y-[-10px] transition-transform duration-150" aria-hidden="true">
+                <div className="w-[6.25rem] h-[6.25rem]">
+                    {svg}
+                </div>
+            </div>
+            <span className="opacity-0 group-hover:opacity-100 absolute bottom-2 left-1/2 transform -translate-x-1/2 duration-150">{skillName}</span>
+        </div>
+    );
+}
+
+SkillCard.propTypes = {
+    skillName: PropTypes.string.isRequired,
+    svg: PropTypes.element.isRequired
+}
