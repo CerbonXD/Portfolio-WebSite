7644260ad725160779d8154a8efb745221a30be2
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2024-09-30 15:29:41 -0300

    Remove use of i18n in Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import ButtonLink from "../components/ButtonLink/ButtonLink.jsx";
 import {useTranslation} from "react-i18next";
 
 export default function Home() {
-    const [t, i18n] = useTranslation();
+    const [t] = useTranslation();
 
     return (
         <section className="flex flex-col justify-center gap-10 h-screen">
