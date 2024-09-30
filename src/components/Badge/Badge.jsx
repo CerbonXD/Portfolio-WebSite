92bed49086b8d1434f29b3e90ebb231a706bc87d
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2024-09-30 00:23:06 -0300

    Remove unnecessary div from Badge.jsx

--- src/components/Badge/Badge.jsx
+++ src/components/Badge/Badge.jsx
@@ -2,10 +2,8 @@ import PropTypes from "prop-types";
 
 export default function Badge({ label, color }) {
     return (
-        <div>
-            <div style={{ backgroundColor: `${color}16`, color: color }} className="inline-block uppercase text-[0.75rem] px-1 rounded-sm">
-                {label}
-            </div>
+        <div style={{ backgroundColor: `${color}16`, color: color }} className="inline-block uppercase text-[0.75rem] px-1 rounded-sm">
+            {label}
         </div>
     );
 }
