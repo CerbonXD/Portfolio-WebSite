50a97aa1c820ee717fc1197606a3b0846becf3c8
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2024-09-30 23:32:57 -0300

    Add show more button translation

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -89,7 +89,7 @@ export default function Projects() {
                         <button
                             className="px-5 py-2 rounded-lg bg-white/[.04] hover:brightness-90"
                             onClick={handleShowMore}>
-                            Show More
+                            {t("projects.show_more.button")}
                         </button>
                     </div>
                 </motion.div>
