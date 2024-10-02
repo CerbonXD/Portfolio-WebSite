759bc15d8036a512cb302205313302b4379ad84b
CerbonXD <felipecerboncinix@gmail.com>
2024-10-01 21:30:43 -0300

    Change home text color

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
                 {t("home.greeting")}
                 <span className="block text-[#6A1FC0] text-[3.375rem]">{t("home.description")}</span>
             </h1>
-            <p className="text-2xl text-[#718096] text-center">{t("home.invite")}</p>
+            <p className="text-2xl text-black/[.65] dark:text-[#718096] text-center">{t("home.invite")}</p>
             <div className="flex gap-5 justify-center">
                 <ButtonLink label="Linkedin" to="https://www.linkedin.com/in/felipe-cerboncini-cordeiro"></ButtonLink>
                 <ButtonLink label="Discord" to="https://discord.gg/zSbSPpunyJ"></ButtonLink>
