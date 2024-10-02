3a4dd463ce3f4b08b74563968bc7906ca3bd7a7f
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2024-10-02 17:59:01 -0300

    Add scale transition to filter button and show more button

--- src/components/FilterButton/FilterButton.jsx
+++ src/components/FilterButton/FilterButton.jsx
@@ -2,9 +2,10 @@ import PropTypes from "prop-types";
 
 export default function FilterButton({ label, isActive, onClick }) {
     const buttonHoverCss = isActive ? " hover:bg-[#6A1FC0]/[.14]" : " hover:bg-black/[.08] dark:hover:bg-white/[.08]"
+    const buttonClickedCss = " active:scale-[.97]"
 
-    const buttonCss = "border-black/[.16] dark:border-white/[.16] border px-5 py-2 rounded-lg bg-none transition" + buttonHoverCss
-    const buttonActiveCss = "border-[#6A1FC0] border px-5 py-2 rounded-lg bg-none text-[#6A1FC0] transition" + buttonHoverCss
+    const buttonCss = "border-black/[.16] dark:border-white/[.16] border px-5 py-2 rounded-lg bg-none transition" + buttonHoverCss + buttonClickedCss
+    const buttonActiveCss = "border-[#6A1FC0] border px-5 py-2 rounded-lg bg-none text-[#6A1FC0] transition" + buttonHoverCss + buttonClickedCss
 
     return (
         <button onClick={onClick} className={isActive ? buttonActiveCss : buttonCss}>
