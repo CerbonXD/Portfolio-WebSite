ac73d14d03df79e4c6fe5ca6cddc96e960d06e86
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2024-10-04 11:36:36 -0300

    Add home main section responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,13 @@ export default function Home() {
     const [t] = useTranslation();
 
     return (
-        <section className="flex flex-col justify-center gap-10 h-screen">
+        <section className="flex flex-col justify-center gap-10 h-screen px-5 lg:px-20">
             <h1 className="text-6xl text-center">
                 {t("home.greeting")}
                 <span className="block text-[#6A1FC0] text-[3.375rem]">{t("home.description")}</span>
             </h1>
             <p className="text-2xl text-[#718096] text-center">{t("home.invite")}</p>
-            <div className="flex gap-5 justify-center">
+            <div className="flex flex-wrap gap-5 justify-center">
                 <ButtonLink label="Linkedin" to="https://www.linkedin.com/in/felipe-cerboncini-cordeiro"></ButtonLink>
                 <ButtonLink label="Discord" to="https://discord.gg/zSbSPpunyJ"></ButtonLink>
             </div>
